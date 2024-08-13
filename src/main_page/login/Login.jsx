1ec10da40ae106dba0c85ccca964c53cc137f141
Subash Lamichhane <subash.lamichhane@icloud.com>
2024-08-13 13:44:26 +1000

    Connecting to backend

--- src/main_page/login/Login.jsx
+++ src/main_page/login/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:8080/login",
+        "https://backend-cd5e.onrender.com/login",
         {
           ...inputValue,
         },
--- src/main_page/signup/Signup.jsx
+++ src/main_page/signup/Signup.jsx
@@ -33,7 +33,7 @@ function Signup() {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:8080/signup",
+        "https://backend-cd5e.onrender.com/signup",
         {
           ...inputValue,
         },
